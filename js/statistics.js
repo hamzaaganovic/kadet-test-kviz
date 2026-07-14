33140a52ea3bc8b653e9040ed8d1f6b4890e8263
hamzaaganovic <hamza.aganovic@gmail.com>
2026-07-14 21:33:21 +0200

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,1 +1,45 @@
+// Statistika aplikacije
 
+
+let statistika = {
+
+    ukupnoTacnih:0,
+
+    ukupnoNetacnih:0,
+
+    ukupnoPitanja:0,
+
+    nizDana:0,
+
+    ukupnoVrijeme:0
+
+};
+
+
+
+function sacuvajStatistiku(){
+
+    sacuvajPodatke(
+        "kadet-statistika",
+        statistika
+    );
+
+}
+
+
+
+function ucitajStatistiku(){
+
+    const podaci =
+    ucitajPodatke(
+        "kadet-statistika"
+    );
+
+
+    if(podaci){
+
+        statistika = podaci;
+
+    }
+
+}
