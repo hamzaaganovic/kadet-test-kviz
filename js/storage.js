e7448f5a27766325d1c0103457ca0ce010aa7667
hamzaaganovic <hamza.aganovic@gmail.com>
2026-07-14 21:32:52 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,25 @@
+// Čuvanje podataka aplikacije
 
+function sacuvajPodatke(naziv, podaci) {
+
+    localStorage.setItem(
+        naziv,
+        JSON.stringify(podaci)
+    );
+
+}
+
+
+function ucitajPodatke(naziv) {
+
+    const podaci = localStorage.getItem(naziv);
+
+    if (!podaci) {
+
+        return null;
+
+    }
+
+    return JSON.parse(podaci);
+
+}
