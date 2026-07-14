90ddaa5d626729cb8d1af27dd7adf15eaf8de415
hamzaaganovic <hamza.aganovic@gmail.com>
2026-07-14 22:05:50 +0200

    Create settings.js

---
+++ js/js/settings.js
@@ -0,0 +1,19 @@
+// Postavke aplikacije
+
+const APP_SETTINGS = {
+
+    naziv: "Kadet Test Kviz",
+
+    verzija: "1.0.0",
+
+    autor: "Hamza Aganovic",
+
+    ukupnoPitanja: 0,
+
+    darkMode: false,
+
+    zvuk: true,
+
+    vibracija: true
+
+};
